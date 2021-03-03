ac49cabe36887810293293cd3451a50b91ff1485
gmy <1508594767@qq.com>
2021-03-03 22:30:37 +0800

    找单链表的中间节点

---
+++ 单链表/链表的中间节点.c
@@ -0,0 +1,21 @@
+
+struct ListNode {
+     int val;
+     struct ListNode *next;
+ };
+
+struct ListNode* middleNode(struct ListNode* head)
+{
+if (head == NULL)
+{
+    return head;
+}
+struct ListNode* slow = head;
+struct ListNode* fast = head;
+while (fast != NULL && fast->next != NULL)
+{
+    slow = slow->next;
+    fast = fast->next->next;
+}
+return slow;
+}
